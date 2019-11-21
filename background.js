f86addaaa5cd39e600d05f527b551c7b56611ca5
markcnunes <mark.mkx@gmail.com>
2019-11-21 06:00:10 +0000

    second commit

--- background.js
+++ background.js
@@ -1,33 +1,35 @@
 function genericOnClick(info, tab) {
-    chrome.tabs.executeScript(null,
-                              {"file": "jquery.min.js"},
-                              function() {
-                                  chrome.tabs.sendMessage(tab.id,{"message":"hide"});
-                              });
-}
-function genericOnClick2(info, tab) {
-    chrome.tabs.executeScript(null,
-                              {"file": "jquery.min.js"},
-                              function() {
-                                  if (info.menuItemId === "contextId1"){
-                                    chrome.tabs.sendMessage(tab.id,{"message":"hide"});
-                                    // var sText = info.selectionText;
-                                    // var url = "https://www.google.com/search?q=" + encodeURIComponent(sText); 
-                                    // window.open(url, '_blank');
-                                  }
-                                  if (info.menuItemId === "contextId2"){
-                                    chrome.tabs.sendMessage(tab.id,{"message":"pageId"});
-                                     
-                                  }
-                              });
+    chrome.tabs.executeScript(
+        null,
+        {
+            file: "jquery.min.js"
+        },
+        function() {
+            if (info.menuItemId === "contextId1") {
+                chrome.tabs.sendMessage(tab.id, {
+                    message: "pageId"
+                });
+            }
+            if (info.menuItemId === "contextId2") {
+                chrome.tabs.sendMessage(tab.id, {
+                    message: info
+                });
+                console.log("Info", info);
+                console.log("Tab", tab);
+            }
+        }
+    );
 }
 
-
-chrome.contextMenus.create({"title": "Hide body",
-                            "contexts":["page"],
-                            "id":"contextId1"});
-chrome.contextMenus.create({"title": "Open this page in the backoffice",
-                            "contexts":["page"],
-                            "id":"contextId2"});
+chrome.contextMenus.create({
+    title: "Open this page in the backoffice",
+    contexts: ["page"],
+    id: "contextId1"
+});
+chrome.contextMenus.create({
+    title: "Open panel",
+    contexts: ["page"],
+    id: "contextId2"
+});
 
 chrome.contextMenus.onClicked.addListener(genericOnClick);
--- content.js
+++ content.js
@@ -1,18 +1,25 @@
-chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-    if (msg.message == 'hide') {
+chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse, info) {
+    if (msg.message == "pageId") {
+        console.log("pageId");
+        // $(document).on("click", function() {
+        // });
+        const bodyClasses = $("body").attr("class");
+        console.log(bodyClasses);
 
-        // console.log('test');
-        // console.log(msg);
-        // console.log(msg.message);
-        $("body").hide();
-        
-    } else if (msg.message == 'pageId'){
-        
-        $(document).on("click", function () {
-            console.log('test');
-            // var clickedBtnID = $(this).attr('id'); // or var clickedBtnID = this.id
-            // alert('you clicked on button #' + clickedBtnID);
-         });
+        const bodyIdClass = bodyClasses.split(" ");
+        const bodyId = bodyIdClass[0].split("body--id-")[1];
+        var url = `https://showoff.asp.events/cmsadmin/pages/editor/contentID/${bodyId}`;
+        window.open(url, "_blank");
+    } else {
+        console.log("panelId", info);
+        // $(document).on("click", function() {
+        var clickedBtnID = $(this).attr("id"); // or var clickedBtnID = this.id
+        // alert('you clicked on button #' + clickedBtnID);
+        //   var url = "https://www.google.com/search?q=" + encodeURIComponent(sText);
+        //   window.open(url, "_blank");
+        // });
+        // $("body").css("background", "green");
+        // console.log(clickedBtnID);
     }
     sendResponse();
 });
