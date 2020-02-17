46ff8f1f34eecc9b48b76d6130f32d66695940a1
Mark Nunes <mark.nunes@asp.events>
2020-02-17 14:19:42 +0000

    New options - backoffice, menus and dev

--- background.js
+++ background.js
@@ -7,29 +7,57 @@ function genericOnClick(info, tab) {
         function() {
             if (info.menuItemId === "contextId1") {
                 chrome.tabs.sendMessage(tab.id, {
+                    message: "backoffice"
+                });
+            } else if (info.menuItemId === "contextId2") {
+                chrome.tabs.sendMessage(tab.id, {
                     message: "page"
                 });
-            }
-            if (info.menuItemId === "contextId2") {
+                // console.log("Info", info);
+                // console.log("Tab", tab);
+            } else if (info.menuItemId === "contextId3") {
                 chrome.tabs.sendMessage(tab.id, {
                     message: "panels"
                 });
                 // console.log("Info", info);
                 // console.log("Tab", tab);
+            } else if (info.menuItemId === "contextId4") {
+                chrome.tabs.sendMessage(tab.id, {
+                    message: "menus"
+                });
+            } else if (info.menuItemId === "contextId5") {
+                chrome.tabs.sendMessage(tab.id, {
+                    message: "dev"
+                });
             }
         }
     );
 }
 
 chrome.contextMenus.create({
-    title: "Open this page in the backoffice",
+    title: "Open backoffice",
     contexts: ["page"],
     id: "contextId1"
 });
 chrome.contextMenus.create({
-    title: "Show panels",
+    title: "Open this page in the backoffice",
     contexts: ["page"],
     id: "contextId2"
+});
+chrome.contextMenus.create({
+    title: "Show panels",
+    contexts: ["page"],
+    id: "contextId3"
+});
+chrome.contextMenus.create({
+    title: "Show menus",
+    contexts: ["page"],
+    id: "contextId4"
+});
+chrome.contextMenus.create({
+    title: "Show dev info",
+    contexts: ["page"],
+    id: "contextId5"
 });
 
 chrome.contextMenus.onClicked.addListener(genericOnClick);
--- content.js
+++ content.js
@@ -1,16 +1,43 @@
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+
+    // Function: Add sidebar
+    function addSidebar(title) {
+        $("body").prepend(
+            `<div id='asp-ext'>
+                 <div class='asp-ext__header'>
+                     <div class='asp-ext__header__logo' style='background-image: url("${chrome.runtime.getURL('icon128.png')}")'></div>
+                     <div class='asp-ext__header__close'>x</div>
+                 </div>
+                 <h4 class='asp-ext__title'>${title}:</h4>
+                 <ul class='asp-ext__items'></ul>
+             </div>`
+         );
+    };
+
+    // Function: Close sidebar
+    function closeSidebar() {   
+        $(".asp-ext__header__close").on("click", function() {
+            $("#asp-ext").remove();
+        });  
+    };
+        
+    // Backoffice URL
     let backofficeUrl;
     let pageUrl = $(location).attr("href");
-    console.log("page url:", pageUrl);
 
-    // Backoffice URL
     if (pageUrl.toLowerCase().indexOf("local.showoff.com") > 0) {
         backofficeUrl = "http://local.showoff.com";
     } else {
         backofficeUrl = "https://showoff.asp.events";
     }
 
-    if (msg.message == "page") {
+    if (msg.message == "backoffice") {
+        let url = `${backofficeUrl}/cmsadmin/dashboard`;
+        
+        window.open(url, "_blank");
+
+    } else if (msg.message == "page") {
+        
         const bodyClasses = $("body").attr("class");
         const bodyIdClass = bodyClasses.split(" ");
         const bodyId = bodyIdClass[0].split("body--id-")[1];
@@ -18,17 +45,19 @@ chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
 
         // Check page type
         /* Libraries or modules */
-        if (!$('.content__main__body .section').length) {
+        let contentMainBody = $('[class*="content__main__body"]'); // @Note: Flex websites use 'l-content__main__body'
+        if (contentMainBody.find('.section').length == 0) {
+            
             /* Exhibitor List */
-            if ($('.content__main__body .m-exhibitors-list').length) {
+            if (contentMainBody.find('.m-exhibitors-list').length) {
                 url = `${backofficeUrl}/cbExhibitors/exhibitor/index`;
             } 
             /* Seminar List */
-            else if ($('.content__main__body .m-seminar-list').length) {
+            else if (contentMainBody.find('.m-seminar-list').length) {
                 url = `${backofficeUrl}/cbSeminars/sessions/index/library/${bodyId}`;
             } 
             /* Library List */
-            else if ($('.content__main__body ul[class*="library-list"]').length) {
+            else if (contentMainBody.find('ul[class*="library-list"]').length) {
                 url = `${backofficeUrl}/cbLibraries/entries/index/library/${bodyId}`;
             }           
         }
@@ -36,55 +65,110 @@ chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
         else {
             url = `${backofficeUrl}/cmsadmin/pages/editor/contentID/${bodyId}`;
         }
+        
         window.open(url, "_blank");
-    } else {
-        // Check if main panel doesn't exist
-        if (!$("#asp-ext").length) {
-            $("body").prepend(
-               `<div id='asp-ext'>
-                    <div class='asp-ext__header'>
-                        <div class='asp-ext__header__logo' style='background-image: url("${chrome.runtime.getURL('icon128.png')}")'></div>
-                        <div class='asp-ext__header__close'>x</div>
-                    </div>
-                    <h4>Panels:</h4>
-                    <ul class='asp-ext__items'></ul>
-                </div>`
-            );
-            // Add main panel
-            const aspEvents = $("#asp-ext");
+    } else if (msg.message == "panels") {
 
-            // Add buttons
-            $(".panel").each(function() {
-                const panelClasses = $(this).attr("class");
-                const panelId = panelClasses.split("panel--id-")[1];
-                const url = `${backofficeUrl}/cmsadmin/panels/editor/contentID/${panelId}`;
-                let panelParentName = $(this).parent().attr("class").toLowerCase();
-                    
-                if (panelParentName.match(/(?:inner|wrapper|__body)/g)) {
-                    
-                    panelParentName = $(this).closest('div:not(.inner):not(.wrapper):not([class*="__body"]):not(.panel)').attr("class").toLowerCase();
+        // Add main panel
+        addSidebar("Panels");
+        
+        // Add buttons
+        $(".panel").each(function() {
+            const panelClasses = $(this).attr("class");
+            const panelId = panelClasses.split("panel--id-")[1];
+            const url = `${backofficeUrl}/cmsadmin/panels/editor/contentID/${panelId}`;
+            let panelParentName = $(this).parent().attr("class").toLowerCase();
+                
+            if (panelParentName.match(/(?:inner|wrapper|__body)/g)) {
+                
+                panelParentName = $(this).closest('div:not(.inner):not(.wrapper):not([class*="__body"]):not(.panel)').attr("class").toLowerCase();
 
-                    // If there is more than one class
-                    if (panelParentName.match(/(?: )/g)) {
-                        panelParentName = panelParentName.split(' ').join(", ");                       
-                    }
-                    
+                // If there is more than one class
+                if (panelParentName.match(/(?: )/g)) {
+                    panelParentName = panelParentName.split(' ').join(", ");                       
                 }
+            }
 
-                aspEvents.find('.asp-ext__items').append(
+            $("#asp-ext").find('.asp-ext__items').append(
+                `<li class='asp-ext__items__item'>
+                    <a class='asp-ext__items__item__link' href='${url}' target='_blank'><span>Panel #${panelId}</span> ${panelParentName}</a>
+                </li>`
+            );
+        });
+
+        closeSidebar();
+         
+    } else if (msg.message == "menus") {
+        // Add main panel
+        addSidebar("Menus");
+        
+        // Add buttons
+        $(".menu[menuid]").each(function() {
+            const menuId = $(this).attr("menuid");
+            const url = `${backofficeUrl}/cmsadmin/menus/editor/menuID/${menuId}`;
+            // const menuText = menuId.split("panel--id-")[1];
+            let menuParentName;
+
+            if ($(this).closest('nav[data-name]').length) {
+                menuParentName = $(this).closest('nav').attr('data-name');
+            } else {
+                menuParentName = $(this).parent().attr("class").toLowerCase();
+            }
+
+            $("#asp-ext .asp-ext__items").append(
+                `<li class='asp-ext__items__item'>
+                    <a class='asp-ext__items__item__link' href='${url}' target='_blank'><span>Menu #${menuId}</span> ${menuParentName}</a>
+                </li>`
+            );
+        });
+
+        closeSidebar();
+
+    } else if (msg.message == "dev") {
+        // Add main panel
+        addSidebar("Dev Info");
+
+        // CSS
+        $("style").each(function(){
+            let css;
+            let cssDataHref = $(this).attr("data-href");
+            if (typeof cssDataHref !== typeof undefined && cssDataHref !== false) {
+                css = $(this).attr("data-href");
+                // if($(this).attr("data-href").has("__media/__theme/css/");
+                
+                $("#asp-ext .asp-ext__items").append(
                     `<li class='asp-ext__items__item'>
-                        <a class='asp-ext__items__item__link' href='${url}' target='_blank'><span>Panel #${panelId}</span> ${panelParentName}</a>
+                        <div class='asp-ext__items__item__text'>
+                            <span>CSS: </span> ${css}
+                        </div>
                     </li>`
                 );
-            });
+            }
+        });
 
-            // Close panel
-            $(".asp-ext__header__close").on("click", function() {
-                $("#asp-ext").hide();
-            });
-        } else {
-            $("#asp-ext").show();
-        }
+        // Header
+        $.ajax({
+            type: 'POST',
+            url: document.location,
+            success: function(data, textStatus, request){
+                let headers = request.getAllResponseHeaders();
+                // headers = headers.replace(', ', '\n');
+
+                $("#asp-ext .asp-ext__items").append(
+                    `<li class='asp-ext__items__item'>
+                        <div class='asp-ext__items__item__text'>
+                            <span>Server: </span> <p>${headers.trim()}</p>
+                        </div>
+                    </li>`
+                );
+            },
+            error: function (request, textStatus, errorThrown) {
+                console.log('error', request);
+            }
+        });
+
+        closeSidebar();
+        
     }
     sendResponse();
 });
